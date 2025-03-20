518e3dee16883c0a750e6de4a630542415fa26e5
Hinata Kameyama <hk2022.123@gmail.com>
2025-03-20 21:09:39 +0900

    モデルのメソッドをコントローラから呼び出す。料理選択画面実装中。

--- resources/views/selectdish/create.blade.php
+++ resources/views/selectdish/create.blade.php
@@ -5,37 +5,18 @@
 <html lang="ja">
 <body>
     <h1>献立入力</h1>
-        <!-- フラッシュメッセージ -->
+        {{-- フラッシュメッセージ --}}
         @if (session('status'))
         <p>{{ session('status') }}</p>
         @endif
 
     <form action="/selectdish" method="POST">
     @csrf
+    {{-- カテゴリごとにメニュー選択欄を表示 --}}
     @foreach ($dishes as $category => $items)
     	<div>
-            <label for="dish">主食:</label>
-            <select name="stapleDish" id="dish">
-                    <option value="" selected>選択してください。</option>
-                @foreach ($items as $dish)
-                    <option value="{{ $dish->name }}">{{ $dish->name }}</option>
-                @endforeach
-            </select>
-        </div>
-
-        <div>
-            <label for="dish">主菜:</label>
-            <select name="mainDish" id="dish">
-                    <option value="" selected>選択してください。</option>
-                @foreach ($items as $dish)
-                    <option value="{{ $dish->name }}">{{ $dish->name }}</option>
-                @endforeach
-            </select>
-        </div>
-
-        <div>
-            <label for="dish">副菜:</label>
-            <select name="sideDish" id="dish">
+            <label for="dish">{{ $category }}:</label>
+            <select name="category" id="dish">
                     <option value="" selected>選択してください。</option>
                 @foreach ($items as $dish)
                     <option value="{{ $dish->name }}">{{ $dish->name }}</option>
@@ -43,6 +24,7 @@
             </select>
         </div>
     @endforeach
+
         <div>
             <label for="comment">コメント:</label>
             <textarea name="comment" id="comment" rows="4"></textarea>
